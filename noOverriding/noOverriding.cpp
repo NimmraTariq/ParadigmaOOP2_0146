0f9bcf8d80f07f68107ca57ef852a996a261e6ad
NimmraTariq <nimra369tariq@gmail.com>
2025-06-02 19:19:12 +0700

    Output message from derivedClass

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class baseClass
+{
+    public:
+    virtual void perkenalan () final 
+    {
+        cout<< "Halo saya Function dari baseClass" << endl;
+    }
+};
+
+class derivedClass : public baseClass
+{
+    public:
+    void perkenalan() 
+    {
+        cout<< "Halo saya Function dari derivedClass";
+    }
+};
+
+int main ()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+    
+}
