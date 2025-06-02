a81e3201df0e678f5c3f1c0a9334af71d36acbfc
NimmraTariq <nimra369tariq@gmail.com>
2025-06-02 17:35:16 +0700

    Print message "Pesan dari lia" to the console

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -5,5 +5,25 @@ class seseorang
 {
  public :
  virtual void pesan ()  
+ {
+    cout << "Pesan dari seseorang" << endl; 
+ }
+};
 
+class joko : public seseorang
+{
+    public: 
+    void pesan () 
+    {
+        cout << "Pesan dari lia" << endl; 
+    }
+};
+
+class lia : public seseorang
+{
+    public: 
+    void pesan () 
+    {
+        cout << "Pesan dari lia" << endl; 
+    }
 };
